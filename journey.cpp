11220b68256437c6ddc34011d56e45cc7e5fbe6c
jareynolds <jareynolds29@gmail.com>
2023-01-17 00:26:50 +0000

    first commit

---
+++ journey.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main() {
+    printf("Hello, My name is Jake. \n");
+    printf("I am a 25 Year old, based in the United Kingdom. \n");
+    printf("Previously I studied Games Design at Teesside University and worked as a Junior Web Developer. \n");
+    printf("This is my Journey to learning Game Development using C++.");
+}

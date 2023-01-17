cec8957e0b45d089fdefb168610ab5bfe4ea025e
jareynolds <jareynolds29@gmail.com>
2023-01-17 00:44:12 +0000

    Added comments to journey.cpp

--- journey.cpp
+++ journey.cpp
@@ -1,8 +1,18 @@
+/* 
+*  File Name:   journey.cpp
+*  Description: This project prints a basic Bio about myself and my prospects to the terminal
+*  Created By:  Jake Reynolds
+*/
 #include <cstdio>
-
-int main() {
+// The Main Function
+int main() // The Function Header
+{
+    // Prints out my name
     printf("Hello, My name is Jake. \n");
+    // Prints out my ages and location
     printf("I am a 25 Year old, based in the United Kingdom. \n");
+    // Prints out my background history
     printf("Previously I studied Games Design at Teesside University and worked as a Junior Web Developer. \n");
+    // Prints out my final comment
     printf("This is my Journey to learning Game Development using C++.");
 }
